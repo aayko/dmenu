da9ff03fffe8eb0a6338ca195d20639a39180ea7
aayko <aayko.dev@pm.me>
2023-12-24 00:26:11 +0100

    Upload repo

---
+++ config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Mononoki Nerd Font:size=13"
+};
+static const int user_bh = 4;               /* add an defined amount of pixels to the bar height */
+
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#080808" },
+	[SchemeUnselected] = { "#aaaaaa", "#080808" },
+	[SchemeSel] = { "#ffffff", "#080808" },
+	[SchemeOut] = { "#080808", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
